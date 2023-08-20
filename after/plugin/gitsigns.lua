fcbd49eed9e76f19d7157c633de9c496b8b475f5
Jay Newey <jayluxray@gmail.com>
2023-08-20 10:26:29 +0100

    Add gitsigns mappings

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -8,6 +8,31 @@ require('gitsigns').setup(
 						changedelete = { text = '~' },
 						untracked    = { text = '┋' },
 				},
+				on_attach = function(bufnr) 
+						local gs = package.loaded.gitsigns
+
+						local function map(mode, l, r, opts)
+								opts = opts or {}
+								opts.buffer = bufnr
+								vim.keymap.set(mode, l, r, opts)
+						end
+
+						-- Navigation
+						map('n', ']c', function()
+								if vim.wo.diff then return ']c' end
+								vim.schedule(function() gs.next_hunk() end)
+								return '<Ignore>'
+						end, {expr=true})
+
+						map('n', '[c', function()
+								if vim.wo.diff then return '[c' end
+								vim.schedule(function() gs.prev_hunk() end)
+								return '<Ignore>'
+						end, {expr=true})
+
+						map('n', '<leader>hp', gs.preview_hunk)
+						map('n', '<leader>tb', gs.toggle_current_line_blame)
+				end
 		}
 )
 
