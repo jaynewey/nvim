727cca8d35de1250d7e0ab8ca41bb7e9a5a48770
Jay Newey <jayluxray@gmail.com>
2023-11-27 19:24:01 +0000

    Update telescope

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -7,8 +7,7 @@ return require("packer").startup(function(use)
 
 		-- fuzzy finder
 		use {
-				"nvim-telescope/telescope.nvim", tag = "0.1.1",
-				-- or                            , branch = "0.1.x",
+				"nvim-telescope/telescope.nvim", tag = "0.1.4",
 				requires = { {"nvim-lua/plenary.nvim"} }
 		}
 
