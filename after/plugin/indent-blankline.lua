b085e8bd40e9ce5525ae7a277bd9458f35422a80
Jay Newey <jayluxray@gmail.com>
2023-11-27 19:21:03 +0000

    update ibl to 3

--- after/plugin/indent-blankline.lua
+++ after/plugin/indent-blankline.lua
@@ -1,9 +1,6 @@
 local pywal_core = require('pywal.core')
 local colors = pywal_core.get_colors()
 
-vim.api.nvim_set_hl(0, "IndentBlanklineChar", {fg=colors.color8})
+vim.api.nvim_set_hl(0, "IblIndent", {fg=colors.color8})
 
-require("indent_blankline").setup {
-    -- for example, context is off by default, use this to turn it on
-    show_current_context = true,
-}
+require("ibl").setup {}
